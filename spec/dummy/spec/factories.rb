b694edcd68fe0acbf1e266826dc4b63d841a730b
jereinhardt <joshuareinhardt@joshuas-mbp.lan>
2020-12-15 15:52:04 -0600

    update test drivers and dependencies

--- spec/dummy/spec/factories.rb
+++ spec/dummy/spec/factories.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "email-#{n}@gmail.com" }
     sequence(:first_name) { |n| "Joe-#{n}"}
-    last_name "Shmo"
+    last_name { "Shmo" }
   end
 end
